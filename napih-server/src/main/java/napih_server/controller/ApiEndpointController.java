f472378b30abb82e9b483e1c13d7b3844458e95b
Hardik Kankane <98350448+hard02@users.noreply.github.com>
2026-04-11 20:05:06 +0000

    removed unsed import to the ApiEndpointController.java file

--- napih-server/src/main/java/napih_server/controller/ApiEndpointController.java
+++ napih-server/src/main/java/napih_server/controller/ApiEndpointController.java
@@ -1,18 +1,22 @@
 package napih_server.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import napih_server.model.ApiEndpoint;
-import napih_server.service.ApiEndpointService;
-import napih_server.dto.response.ApiEndpointResponse;
 import napih_server.dto.request.RegisterApiRequest;
 import napih_server.dto.request.UpdateApiRequest;
-
-
-
-import java.util.List;
+import napih_server.dto.response.ApiEndpointResponse;
+import napih_server.service.ApiEndpointService;
 
 @RestController
 @RequestMapping("/api/endpoints")
